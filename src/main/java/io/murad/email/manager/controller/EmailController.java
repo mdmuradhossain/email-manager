f9ca7f73282901aa61601354bcacf71db8c150ec
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-08 19:58:17 +0600

    email sent successfully

--- src/main/java/io/murad/email/manager/controller/EmailController.java
+++ src/main/java/io/murad/email/manager/controller/EmailController.java
@@ -1,6 +1,8 @@
 package io.murad.email.manager.controller;
 
 import io.murad.email.manager.model.Email;
+import io.murad.email.manager.service.MailService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value ="/mail")
+@AllArgsConstructor
 public class EmailController {
 
+    private final MailService mailService;
 
     @GetMapping(path = "/form")
     public String emailForm(Model model){
@@ -21,6 +25,7 @@ public class EmailController {
     }
     @PostMapping(path = "/send")
     public String send(@ModelAttribute("email") Email email){
+        mailService.sendMail(email);
         return "email/sentEmails";
     }
 }
