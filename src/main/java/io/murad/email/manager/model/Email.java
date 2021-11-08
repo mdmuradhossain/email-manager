74522ffe08178d2d5f77b71bbe81f7cbcfb829ae
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-08 20:09:46 +0600

    JPA added to model

--- src/main/java/io/murad/email/manager/model/Email.java
+++ src/main/java/io/murad/email/manager/model/Email.java
@@ -4,13 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+
 @Data
+@Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 public class Email {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "from")
     private String from;
+
+    @Column(name = "to")
     private String to;
+
+    @Column(nullable = true)
     private String cc;
+
+    @Column(name = "subject")
     private String subject;
+
+    @Column(name = "body")
+    @Lob
     private String body;
 }
