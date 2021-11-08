3d28d5c68a992b4f73b2e88d8d29c40fc44883e7
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-08 13:57:15 +0600

    Email model added

---
+++ src/main/java/io/murad/email/manager/model/Email.java
@@ -0,0 +1,15 @@
+package io.murad.email.manager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Email {
+    private String from;
+    private String to;
+    private String cc;
+    private String body;
+}
