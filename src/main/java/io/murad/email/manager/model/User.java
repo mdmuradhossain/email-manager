4176b14c4045a3e12b3fe2be61bcdf89de6d518e
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-18 22:31:53 +0600

    User model added

---
+++ src/main/java/io/murad/email/manager/model/User.java
@@ -0,0 +1,23 @@
+package io.murad.email.manager.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String fullname;
+}
